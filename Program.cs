fe907152adbfc36354e0cb38c81f241ee5ba3677
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-04-05 12:59:33 -0300

    Programa de divisão finalizado

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Divisao_DecisaoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool denominadorValido;
+            int novoDenominador;
+
+            Console.WriteLine("--- Divisão ---\n");
+
+            Console.Write("Digite um numerador: ");
+            int numeradorDigitado = Convert.ToInt32(Console.ReadLine());
+
+             Console.Write("Digite um denominador: ");
+            string denominadorDigitado = Console.ReadLine();
+
+            denominadorValido = Int32.TryParse(denominadorDigitado,out novoDenominador);
+
+            if (novoDenominador == 0)
+            {
+                Console.WriteLine("Não é possível dividir por zero, tente novamente!");
+            }
+            else
+            {
+            int resultadoDivisao = numeradorDigitado / novoDenominador;
+            Console.WriteLine($"O resultado da divisão é: {resultadoDivisao}.");
+            }
+
+
+
+         
+        
+
+            
+            
+
+            
+        }
+    }
+}
